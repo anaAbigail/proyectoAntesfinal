0219b84cfdefe428fb76183f3f2d0caa5ae92444
YadhiPrograma <yadhiprogramacion@gmail.com>
2023-09-07 12:22:13 -0300

    Update ProyectoAntesfinal.java

--- src/proyectoantesfinal/ProyectoAntesfinal.java
+++ src/proyectoantesfinal/ProyectoAntesfinal.java
@@ -1,13 +1,13 @@
 
 package proyectoantesfinal;
 
-
 public class ProyectoAntesfinal {
 
     public static void main(String[] args) {
         // hola Enzo
         
         System.out.println("Holaaa");
+        System.out.println("EAAAAAA");
     }
     
 }
